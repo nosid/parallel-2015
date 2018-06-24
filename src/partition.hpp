ef3d81cf63a4b3c0b07d709b664146169df018d2
Hubert Schmid <hubert.schmid@nosid.de>
2018-06-24 20:24:53 +0100

    add static_assert to make sure that the types for partitioner are integral types

--- src/partition.hpp
+++ src/partition.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <type_traits>
+
 namespace demo
 {
 
     template <typename Amount, typename Parts>
     auto partitioner(Amount amount, Parts parts)
     {
+        static_assert(std::is_integral_v<Amount>);
+        static_assert(std::is_integral_v<Parts>);
         return [parts,amount]() mutable {
             auto result = amount / parts;
             --parts;
